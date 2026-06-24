92178015c4cb7f60e0d144790240973016c4ef79
khalnetLaMoula <khalnet92t@gmail.com>
2026-06-24 18:30:10 +0200

    refacto a cause d'une betose

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,23 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@pinia/nuxt']
+  modules: ['@nuxt/eslint', '@pinia/nuxt'],
+  
+  pinia: {
+    storesDirs: ['./app/stores/**'],
+  },
+
+  // on déclare l'URL de l'API ici (propre + réutilisable)
+  runtimeConfig: {
+    public: {
+      pokeApiBase: 'http://localhost:3000/api',
+    },
+  },
+
+  eslint: {
+    config: {
+      stylistic: true
+    }
+  }
 })
+
