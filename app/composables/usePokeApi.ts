8366365c2e9b843544eb6764860dd12ed4ce9d22
khalnetLaMoula <khalnet92t@gmail.com>
2026-06-24 17:46:27 +0200

    Config Pinia (@pinia/nuxt), config d'un client HTTP pour PokéAPI (un wrapper $fetch).

---
+++ app/composables/usePokeApi.ts
@@ -0,0 +1,16 @@
+export const useUsePokeApi = () => {
+  const config = useRuntimeConfig();
+
+  const pokeApi = $fetch.create({
+    baseURL: config.public.pokeApiBase as string,
+
+    onRequestError({ error }) {
+      console.error('Error fetching PokeAPI data:', error);
+    },
+
+    onResponseError({ response }) {
+      console.error('Error response from PokeAPI:', response);
+    }
+  });
+  return { pokeApi }
+}
--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,6 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxt/eslint', '@pinia/nuxt'],
+
+  pinia: {
+    storesDirs: ['./app/stores/**'],
+  },
+
+  // on déclare l'URL de l'API ici (propre + réutilisable)
+  runtimeConfig: {
+    public: {
+      pokeApiBase: 'http://localhost:3000/api',
+    },
+  },
+
   eslint: {
     config: {
       stylistic: true
