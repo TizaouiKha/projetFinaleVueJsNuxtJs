01475396aca92323a96e30e75cd079da7470875d
Wilfried Bour <wilfried.bourguignon@epitech.eu>
2026-07-15 15:23:33 +0200

    Project init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  modules: [
+    '@pinia/nuxt'
+  ],
+
+  css: [
+    '~/assets/css/main.css'
+  ],
+
+  vite: {
+    plugins: [
+      tailwindcss()
+    ]
+  }
 })
